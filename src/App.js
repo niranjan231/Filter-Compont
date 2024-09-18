7637c1d6e1e641bfe98c97e822a83341fd86a833
hp <niranjan962796@gmail.com>
2024-09-18 18:58:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data';
+import {useState} from "react";
+
 
 function App() {
+  const [item2 , setItem2] = useState(data)
+  const UpdateItem=(carditem)=>{
+const filterProduct = data.filter((curditem4)=>{
+  return curditem4.category===carditem
+})
+setItem2(filterProduct)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={()=>setItem2(data)} >All</button>
+     <button onClick={()=>UpdateItem("men's clothing")}>men's clothing</button>
+     <button onClick={()=>UpdateItem("women's clothing")}>Women</button>
+     <button onClick={()=>UpdateItem("jewelery")}>jewelery</button>
+     {/* <button onClick={}>jewelery</button> */}
+<div className="app-2">
+  {
+    item2.map((item)=>{
+      return <div class="card" style={{width: "18rem"}}>
+      <img src={item.image} class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">{item.category}</h5>
+        <a href="#" class="btn btn-primary"></a>
+      </div>
+    </div>
+    })
+  }
+</div>
+
     </div>
   );
 }
